45c994fd91e22432407a741075de44f46c2a9869
Wrench56 <dmarkreg@gmail.com>
2023-02-09 04:37:46 +0100

    Add manualcurl.sh tool for testing lichess.org API

---
+++ tools/manualcurl.sh
@@ -0,0 +1,28 @@
+if [ -z "$1" ] || [ -z "$2" ]
+then
+    echo "Usage: ./manualcurl.sh <api_token> <mode>"
+fi
+
+if [ $2 = 1 ]
+then
+    echo "User info:"
+    curl https://lichess.org/api/account -H "Authorization: Bearer $1"
+    echo
+elif [ $2 = 2 ]
+then
+    echo "Currently playing:"
+    curl https://lichess.org/api/account/playing -H "Authorization: Bearer $1"
+    echo
+elif [ $2 = 3 ] 
+then
+    if [ -z "$3" ]
+    then
+        echo "Usage: ... <username_to_challenge>"
+    else
+        echo "Challenging $3..."
+        curl https://lichess.org/api/challenge/$3 -H "Authorization: Bearer $1" "Content-Type: application/x-www-form-urlencoded" -d "" -o /dev/null -s
+        echo
+    fi
+else
+    echo "Error: $2 is not a valid mode!"
+fi
