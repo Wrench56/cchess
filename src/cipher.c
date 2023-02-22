e5e5b8064a8489ac7fba37063ea025364dd0bfb1
Wrench56 <dmarkreg@gmail.com>
2023-02-22 16:43:44 +0100

    Change debug value in XOR cipher

--- src/cipher.c
+++ src/cipher.c
@@ -2,7 +2,7 @@
 
 char* xor_cipher(char* data, char* key, int dataLen, int keyLen) {
 	char* output = (char*)malloc(sizeof(char) * dataLen + 1);
-	output[24] = '\0';
+	output[sizeof(char) * dataLen] = '\0';
 
 	for (int i = 0; i < dataLen; ++i) {
 		output[i] = data[i] ^ key[i % keyLen];
