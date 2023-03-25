aed1fe8f0b912b57591fcc5609e6136d74a70898
Wrench56 <dmarkreg@gmail.com>
2023-03-25 22:59:21 +0100

    Add threads for game.c & GameState struct

--- src/game/game.c
+++ src/game/game.c
@@ -1,5 +1,33 @@
+#include <ncurses.h>
+#include <pthread.h>
+#include <string.h>
+
 #include "../api.h"
 
-void game_stream(char* game_id) {
+#include "game.h"
+
+
+
+/* API thread */
+void* read_api(void* game_state_ptr) {
+    read_game_stream(game_state_ptr);
+}
+
+/* Main thread (Input thread) */
+void game_stream(char* api_key, char* game_id) {
+    /* Set up API thread */
+    pthread_t api_thread;
+    struct GameState game_state;
+
+    strcpy(game_state.api_key, api_key);
+    strcpy(game_state.game_id, game_id);
+
+    pthread_create(&api_thread, NULL, read_api, &game_state);
     
+    /* Setup UI */
+    clear();
+    
+    refresh();
+    getch();
+
 }
--- src/game/game.h
+++ src/game/game.h
@@ -1,3 +1,8 @@
 #pragma once
 
-void game_stream(char* game_id);
+struct GameState {
+    char api_key[25]; 
+    char game_id[9];
+};
+
+void game_stream(char* api_key, char* game_id);
