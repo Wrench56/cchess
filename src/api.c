d94ffcd3696b1e276cb3b863356de07e4bb48834
Wrench56 <dmarkreg@gmail.com>
2023-03-25 22:58:11 +0100

    Add game stream reading (api.c)

--- src/api.c
+++ src/api.c
@@ -3,8 +3,9 @@
 
 #include <curl/curl.h>
 #include <cjson/cJSON.h>
+#include <ncurses.h>
 
-#include "api.h"
+#include "game/game.h"
 
 struct MemoryStruct {
   char *memory;
@@ -44,7 +45,7 @@ cJSON* get_request(char* api_key, char* url) {
   
 
     curl = curl_easy_init();
-    char auth_header[46] = "Authorization: Bearer ";
+    char auth_header[47] = "Authorization: Bearer ";
     
     if (curl) {
         curl_easy_setopt(curl, CURLOPT_URL, url);
@@ -69,5 +70,66 @@ cJSON* get_request(char* api_key, char* url) {
         return result;
     } else {
         return cJSON_Parse("{\"fail\": 1}");
+    }
+}
+
+
+
+size_t parse_game_stream(char* buffer, size_t size, size_t nmemb, void* userp)
+{
+    size_t realsize = size * nmemb;
+    char data[realsize + 1];
+    memcpy(data, buffer, realsize);
+    if (data < 2) { // Check for keep-alive
+        return realsize;
+    }
+
+    cJSON* json = cJSON_Parse(data);
+    if (json == NULL) {
+        printw("Error: Can't parse JSON!");
+        return realsize;
+    }
+
+    cJSON* type = cJSON_GetObjectItemCaseSensitive(json, "type");
+    
+    if (type->valuestring == "gameState") {
+        cJSON* moves = cJSON_GetObjectItemCaseSensitive(json, "");
+    } else if (type->valuestring == "opponentGone") {
+
+    } else if (type->valuestring == "gameFull") {
+
+    } else { // chatLine
+        
+    }
+
+    return realsize;
+}
+
+
+void read_game_stream(void* game_state_ptr) {
+    CURL *curl;
+    CURLcode res;
+    struct curl_slist *list = NULL;
+    struct GameState* game_state = (struct GameState*)game_state_ptr;
+
+    curl = curl_easy_init();
+    char auth_header[47] = "Authorization: Bearer ";
+    char url[51] = "https://lichess.org/api/board/game/stream/";
+    
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, strcat(url, game_state->game_id));
+        list = curl_slist_append(list, strcat(auth_header, game_state->api_key));
+        list = curl_slist_append(list, "Accept:");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, parse_game_stream);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, NULL);
+ 
+    
+        res = curl_easy_perform(curl);
+
+        /* always cleanup */
+        curl_slist_free_all(list);
+        curl_easy_cleanup(curl);
     }
 }
--- src/api.h
+++ src/api.h
@@ -3,3 +3,4 @@
 #include <cjson/cJSON.h>
 
 cJSON* get_request(char* api_key, char* url);
+void read_game_stream(void* game_state_ptr);
