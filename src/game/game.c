dc85dd1f1c88f1b79619e0f226c4b2006c618768
Wrench56 <dmarkreg@gmail.com>
2023-03-25 23:54:51 +0100

    Add black & white username in Game struct & set starting board

--- src/game/game.c
+++ src/game/game.c
@@ -22,6 +22,9 @@ void game_stream(char* api_key, char* game_id) {
     strcpy(game.api_key, api_key);
     strcpy(game.game_id, game_id);
 
+    /* Set starting board */
+    strcpy(game.board, "rnbqkbnrpppppppp00000000000000000000000000000000PPPPPPPPRNBQKBNR");
+
     pthread_create(&api_thread, NULL, read_api, &game);
     
     /* Setup UI */
--- src/game/game.h
+++ src/game/game.h
@@ -4,6 +4,8 @@ struct Game {
     char api_key[25]; 
     char game_id[9];
     char board[65];
+    char white_name[32];
+    char black_name[32];
 };
 
 void game_stream(char* api_key, char* game_id);
