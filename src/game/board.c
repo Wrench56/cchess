eda935e379d3b5c0a20e14f62f0787638e06ad8e
Wrench56 <dmarkreg@gmail.com>
2023-03-31 19:47:06 +0200

    Upgrade show_board() function to show colors & pieces

--- src/game/board.c
+++ src/game/board.c
@@ -1,17 +1,66 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
+#include "../settings.h"
+
 #include "game.h"
 
 
 void show_board(struct Game* game, int x, int y) {
+    char char_to_process;
+    short is_black = 0;
+    short is_black_square = 1;
+
     move(y, x);
     short row = 1;
     for (short i = 0; i < 8; i++) {
         for (short j = 0; j < 8; j++) {
-            // I'm not very happy with this solution
-            printw("%c", game->board[i*8+j]);
+            char_to_process = game->board[i*8+j];
+
+            if (is_black_square == 0) {
+                is_black_square = 1;
+                attroff(COLOR_PAIR(1));
+                attroff(COLOR_PAIR(3));
+                attron(COLOR_PAIR(2+is_black));
+            } else {
+                is_black_square = 0;
+                attroff(COLOR_PAIR(2));
+                attroff(COLOR_PAIR(4));
+                attron(COLOR_PAIR(1+is_black));
+            }
+            if (char_to_process >= 'a') {
+                char_to_process -= 32;
+                is_black = 2;
+            }
+            switch (char_to_process) {
+                case 'P': // Pawn
+                    printw(FIGURE_STRING, PAWN_FIGURE);
+                    break;
+                case 'R': // Rook
+                    printw(FIGURE_STRING, ROOK_FIGURE);
+                    break;
+                case 'B': // Bishop
+                    printw(FIGURE_STRING, BISHOP_FIGURE);
+                    break;
+                case 'N': // Knight
+                    printw(FIGURE_STRING, KNIGHT_FIGURE);
+                    break;
+                case 'Q': // Queen
+                    printw(FIGURE_STRING, QUEEN_FIGURE);
+                    break;
+                case 'K': // King
+                    printw(FIGURE_STRING, KING_FIGURE);
+                    break;
+                case '0': // Space
+                    printw(FIGURE_STRING, " ");
+                    break;
+                default:
+                    printw("Er");
+                    break;
+            }
+            is_black = 0;
         }
+        is_black_square = !is_black_square;
         move(y+row, x);
         row++;
     }
