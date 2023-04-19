6241f4382b4f4d69d030a03e383fd451a39ec8b7
Wrench56 <dmarkreg@gmail.com>
2023-04-20 00:08:26 +0200

    Send moves to lichess & add "status" logs

--- src/game/game.c
+++ src/game/game.c
@@ -1,13 +1,63 @@
 #include <ncurses.h>
 #include <pthread.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "../api.h"
 
 #include "board.h"
 #include "game.h"
 
+void report(char* message) {
+    int win_w;
+    int win_h;
+    getmaxyx(stdscr, win_h, win_w);
 
+    int msg_len = 0;
+
+    /* Support string literals for strtok() */
+    char *msg_copy = (char *)malloc(strlen(message) + 1);
+    strcpy(msg_copy, message);
+
+    char* word = strtok(msg_copy, " ");
+
+    while (word != NULL) {
+        if ((strlen(word) + msg_len) > (win_w - 32)) {
+            move(10, 21);
+            msg_len = 0;
+        }
+        printw(" %s", word);
+        msg_len += strlen(word);
+        word = strtok(NULL, " ");
+    }
+}
+
+void report_cleanup() {
+    int win_w;
+    int win_h;
+    getmaxyx(stdscr, win_h, win_w);
+
+    for (int i = 0; i < win_w; i++) {
+        mvaddch(9, 22+i, ' ');
+        mvaddch(10, 22+i, ' ');
+    }
+}
+
+void report_error(char* message) {
+    report_cleanup();
+    attron(COLOR_PAIR(5));
+    mvprintw(9, 22, "Error:");
+    attroff(COLOR_PAIR(5));
+    report(message);
+}
+
+void report_success(char* message) {
+    report_cleanup();
+    attron(COLOR_PAIR(7));
+    mvprintw(9, 22, "Success:");
+    attroff(COLOR_PAIR(7));
+    report(message);
+}
 
 /* API thread */
 void* read_api(void* game_struct_ptr) {
@@ -75,15 +125,28 @@ void game_stream(char* api_key, char* game_id) {
                 refresh();
             }
         } else if (key == '\n') {
-            if (strlen(move_inp) == 2) { // Move select
+            if (strlen(move_inp) == 2) { // Select
                 show_valid_moves(&game, move_inp);
                 show_board(&game, 1, 1);
                 refresh();
 
             } else if (strlen(move_inp) == 4) { // Move
+                char joined_url[54] = "https://lichess.org/api/board/game/";
+                strcat(joined_url, game.game_id);
+                strcat(joined_url, "/move/");
+                strcat(joined_url, move_inp);
 
-            } else { // Error
+                cJSON* returned = http_request(game.api_key, joined_url, "");
+                cJSON* ok = cJSON_GetObjectItemCaseSensitive(returned, "ok");
+                if (ok == NULL) {
+                    cJSON* error = cJSON_GetObjectItemCaseSensitive(returned, "error");
+                    report_error(error->valuestring);
+                } else {
+                    report_success("Piece moved!");
+                }
                 
+            } else { // Error
+                report_error("Invalid move/select!");
             }
             
         }
